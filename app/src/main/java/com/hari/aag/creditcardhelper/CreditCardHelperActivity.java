feb56e19f9a9c88b17e2cba3c94e6ee63abc7e2e
harinivaskumar <harinivaskumar@live.com>
2016-12-11 21:27:45 +0530

    Code implementation

--- app/src/main/java/com/hari/aag/creditcardhelper/CreditCardHelperActivity.java
+++ app/src/main/java/com/hari/aag/creditcardhelper/CreditCardHelperActivity.java
@@ -1,13 +1,231 @@
 package com.hari.aag.creditcardhelper;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class CreditCardHelperActivity extends AppCompatActivity {
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.hari.aag.creditcardhelper.R.id.cardFinalBalance;
+
+public class CreditCardHelperActivity extends AppCompatActivity
+        implements View.OnClickListener {
+
+    private static final String LOG_TAG = CreditCardHelperActivity.class.getSimpleName();
+    private static final String PREFS_NAME = CreditCardHelperActivity.class.getSimpleName();
+
+    private String cardBalanceStr, cardYearlyInterestRateStr, cardMinimumPaymentStr;
+    private String cardFinalBalanceStr, cardMonthsRemainingStr, cardInterestPaidStr;
+
+    private static final String CARD_BALANCE = "cardBalance";
+    private static final String YEARLY_INTEREST_RATE = "cardYearlyInterestRate";
+    private static final String MINIMUM_PAYMENT = "cardMinimumPayment";
+    private static final String FINAL_BALANCE = "cardFinalBalance";
+    private static final String MONTHS_REMAINING = "cardMonthsRemaining";
+    private static final String INTEREST_PAID = "cardInterestPaid";
+
+    @BindView(R.id.cardBalance) EditText cardBalanceET;
+    @BindView(R.id.cardYearlyInterestRate) EditText cardYearlyInterestRateET;
+    @BindView(R.id.cardMinimumPayment) EditText cardMinimumPaymentET;
+    @BindView(cardFinalBalance) EditText cardFinalBalanceET;
+    @BindView(R.id.cardMonthsRemaining) EditText cardMonthsRemainingET;
+    @BindView(R.id.cardInterestPaid) EditText cardInterestPaidET;
+
+    @BindView(R.id.compute) Button computeBtn;
+    @BindView(R.id.clear) Button clearBtn;
+
+    @BindString(R.string.default_final_balance_value) String defaultFinalBalanceValue;
+    @BindString(R.string.default_months_remaining_value) String defaultMonthsRemainingValue;
+    @BindString(R.string.default_interet_paid_value) String defaultInterestPaidValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credit_card_helper);
+        ButterKnife.bind(this);
+
+        computeBtn.setOnClickListener(this);
+        clearBtn.setOnClickListener(this);
+
+        readValuesFromPrefs();
+        updateValueToUI(true);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.compute:
+                double principle_P, minDue_D, rateOfInterest_R, newPrinciple_P;
+                double monthlyInterestAmount_MIA, availableBalance_AB;
+                double finalCardBalance_FCB = 0, totalInterestPaid_TIP = 0;
+                int noOfMonths_N = 0;
+
+                String cardBalanceStr1 = cardBalanceET.getText() + "";
+                if (cardBalanceStr1.isEmpty()){
+                    Toast.makeText(this, "Card Balance is Empty!", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+
+                String cardYearlyInterestRateStr1 = cardYearlyInterestRateET.getText() + "";
+                if (cardYearlyInterestRateStr1.isEmpty()){
+                    Toast.makeText(this, "Card Yearly Interest Rate is Empty!", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+
+                String cardMinimumPaymentStr1 = cardMinimumPaymentET.getText() + "";
+                if (cardMinimumPaymentStr1.isEmpty()){
+                    Toast.makeText(this, "Card Minimum Payment is Empty!", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+
+                if ((!cardBalanceStr.isEmpty() && cardBalanceStr.equals(cardBalanceStr1) ||
+                        (!cardYearlyInterestRateStr.isEmpty() && cardYearlyInterestRateStr.equals(cardYearlyInterestRateStr1)) ||
+                        (!cardMinimumPaymentStr.isEmpty() && cardMinimumPaymentStr.equals(cardMinimumPaymentStr1)))){
+                    Log.d("CreditCardHelperAct", "Calculation Skipped.\n");
+                    break;
+                }
+
+                cardBalanceStr = cardBalanceStr1;
+                cardYearlyInterestRateStr = cardYearlyInterestRateStr1;
+                cardMinimumPaymentStr = cardMinimumPaymentStr1;
+
+                principle_P = Double.parseDouble(cardBalanceStr1);
+                rateOfInterest_R = Double.parseDouble(cardYearlyInterestRateStr1);
+                minDue_D = Double.parseDouble(cardMinimumPaymentStr1);
+
+                newPrinciple_P = principle_P;
+                do {
+                    monthlyInterestAmount_MIA = ((newPrinciple_P * rateOfInterest_R) / (100 * 12));
+
+                    availableBalance_AB = newPrinciple_P - minDue_D + monthlyInterestAmount_MIA;
+
+                    if (noOfMonths_N == 0)
+                        finalCardBalance_FCB = availableBalance_AB;
+
+                    totalInterestPaid_TIP += monthlyInterestAmount_MIA;
+
+                    noOfMonths_N++;
+
+                    newPrinciple_P = availableBalance_AB;
+                } while (availableBalance_AB > 0);
+
+                /*
+                cardFinalBalanceStr = finalCardBalance_FCB + "";
+                cardInterestPaidStr = totalInterestPaid_TIP + "";
+                */
+
+                cardFinalBalanceStr = getFormattedDouble(finalCardBalance_FCB);
+                cardMonthsRemainingStr = noOfMonths_N + "";
+                cardInterestPaidStr = getFormattedDouble(totalInterestPaid_TIP);
+
+                updateValueToUI(false);
+                saveValuesToPrefs();
+                /*
+                saveValuesToPrefs( new String[] {
+                        cardBalanceStr, cardYearlyInterestRateStr, cardMinimumPaymentStr,
+                        cardFinalBalanceStr, cardMonthsRemainingStr, cardInterestPaidStr
+                });
+                */
+
+                break;
+            case R.id.clear:
+                cardBalanceStr = "";
+                cardYearlyInterestRateStr = "";
+                cardMinimumPaymentStr = "";
+                cardFinalBalanceStr = defaultFinalBalanceValue;
+                cardMonthsRemainingStr = defaultMonthsRemainingValue;
+                cardInterestPaidStr = defaultInterestPaidValue;
+
+                updateValueToUI(true);
+                saveValuesToPrefs();
+                /*
+                saveValuesToPrefs( new String[] {
+                        cardBalanceStr, cardYearlyInterestRateStr, cardMinimumPaymentStr,
+                        cardFinalBalanceStr, cardMonthsRemainingStr, cardInterestPaidStr
+                });
+                */
+                break;
+        }
+    }
+
+    private String getFormattedDouble(Double value){
+        return String.format("%.2f", value);
+    }
+
+    private void updateValueToUI(boolean isInputFieldsReq){
+        if (isInputFieldsReq){
+            cardBalanceET.setText(cardBalanceStr);
+            cardYearlyInterestRateET.setText(cardYearlyInterestRateStr);
+            cardMinimumPaymentET.setText(cardMinimumPaymentStr);
+        }
+        cardFinalBalanceET.setText(cardFinalBalanceStr);
+        cardMonthsRemainingET.setText(cardMonthsRemainingStr);
+        cardInterestPaidET.setText(cardInterestPaidStr);
+    }
+
+    private void readValuesFromPrefs(){
+        SharedPreferences mySharedPrefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+
+        cardBalanceStr = mySharedPrefs.getString(CARD_BALANCE, "");
+        cardYearlyInterestRateStr = mySharedPrefs.getString(YEARLY_INTEREST_RATE, "");
+        cardMinimumPaymentStr = mySharedPrefs.getString(MINIMUM_PAYMENT, "");
+        cardFinalBalanceStr = mySharedPrefs.getString(FINAL_BALANCE, defaultFinalBalanceValue);
+        cardMonthsRemainingStr = mySharedPrefs.getString(MONTHS_REMAINING, defaultMonthsRemainingValue);
+        cardInterestPaidStr = mySharedPrefs.getString(INTEREST_PAID, defaultInterestPaidValue);
+
+        Log.d(LOG_TAG, "Values Read from Prefs.\n");
+        dumpPrefValues();
+    }
+
+    private void saveValuesToPrefs(){
+        SharedPreferences.Editor prefsEditor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+
+        prefsEditor.putString(CARD_BALANCE, cardBalanceStr);
+        prefsEditor.putString(YEARLY_INTEREST_RATE, cardYearlyInterestRateStr);
+        prefsEditor.putString(MINIMUM_PAYMENT, cardMonthsRemainingStr);
+        prefsEditor.putString(FINAL_BALANCE, cardFinalBalanceStr);
+        prefsEditor.putString(MONTHS_REMAINING, cardMonthsRemainingStr);
+        prefsEditor.putString(INTEREST_PAID, cardInterestPaidStr);
+        prefsEditor.commit();
+
+        Log.d(LOG_TAG, "Values Saved to Prefs.\n");
+        dumpPrefValues();
+    }
+
+    private void dumpPrefValues(){
+        Log.d(LOG_TAG, CARD_BALANCE + " - " + cardBalanceStr);
+        Log.d(LOG_TAG, YEARLY_INTEREST_RATE +  " - " + cardYearlyInterestRateStr);
+        Log.d(LOG_TAG, MINIMUM_PAYMENT +  " - " + cardMinimumPaymentStr);
+        Log.d(LOG_TAG, FINAL_BALANCE +  " - " + cardFinalBalanceStr);
+        Log.d(LOG_TAG, MONTHS_REMAINING +  " - " + cardMonthsRemainingStr);
+        Log.d(LOG_TAG, INTEREST_PAID +  " - " + cardInterestPaidStr);
+    }
+
+    /*
+    private void saveValuesToPrefs(String values[]){
+        SharedPreferences.Editor editor = mySharedPrefs.edit();
+        editor.putString(CARD_BALANCE, values[0]);
+        editor.putString(YEARLY_INTEREST_RATE, values[1]);
+        editor.putString(MINIMUM_PAYMENT, values[2]);
+        editor.putString(FINAL_BALANCE, values[3]);
+        editor.putString(MONTHS_REMAINING, values[4]);
+        editor.putString(INTEREST_PAID, values[5]);
+        editor.commit();
+
+        Log.d("CreditCardHelperAct", CARD_BALANCE + values[0]);
+        Log.d("CreditCardHelperAct", YEARLY_INTEREST_RATE + values[1]);
+        Log.d("CreditCardHelperAct", MINIMUM_PAYMENT + values[2]);
+        Log.d("CreditCardHelperAct", FINAL_BALANCE + values[3]);
+        Log.d("CreditCardHelperAct", MONTHS_REMAINING + values[4]);
+        Log.d("CreditCardHelperAct", INTEREST_PAID + values[5]);
+        Log.d("CreditCardHelperAct", "Saved to Prefs.\n");
+    }
+    */
 }
